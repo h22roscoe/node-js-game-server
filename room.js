b693a448cdc24d4e3b9919b1017dc0b652d0d533
Harry <harry.roscoe14@imperial.ac.uk>
2016-05-28 15:58:36 +0100

    Minor stylistic changes to room.js

--- room.js
+++ room.js
@@ -4,19 +4,16 @@ Author: Huy Tran
 Email: kingbazoka@gmail.com
 */
 
-function run(room, player, msg)
-{
-	// Implement your game room (server side) logic here
-	console.log("Processing " + player.name + "@" + room.name + ": " + msg);
+function run(room, player, msg) {
+    // Implement your game room (server side) logic here
+    console.log("Processing " + player.name + "@" + room.name + ": " + msg);
 }
 
-function update(room)
-{
-	// Update room
-	if (room.IsPlaying())
-	{
-		console.log("Room " + room.name + " is playing");
-	}
+function update(room) {
+    // Update room
+    if (room.isPlaying()) {
+        console.log("Room " + room.name + " is playing");
+    }
 }
 
 module.exports.update = update;
